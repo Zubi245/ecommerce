82c438cbcaf4dc904b057f30883db06c7e34a670
Zubi245 <164869031+Zubi245@users.noreply.github.com>
2025-12-25 10:53:51 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    GEMINI_API_KEY: "PLACEHOLDER_API_KEY",
+    MONGO_URI: "mongodb + srv://zubairanwar245_db_user:dKB52OQEFvyQvEK5@cluster0.h7jabyi.mongodb.net/samfabrics",
+
+    IMAGEKIT_PUBLIC_KEY: "public_C+eb90mUDk9SaKV/4otCfze+N+g=",
+    IMAGEKIT_PRIVATE_KEY: "private_vXrOlOMEjxCuucJvyTPoidmZZMw=",
+    IMAGEKIT_URL_ENDPOINT: "https://ik.imagekit.io/samfabrics"
+  }
 };
 
 export default nextConfig;
