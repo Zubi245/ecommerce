2555cac89d4aeb09f4bfb738d28df5c96c4ea6a0
Zubi245 <164869031+Zubi245@users.noreply.github.com>
2025-12-25 11:35:19 +0500

    added .env file

--- next.config.ts
+++ next.config.ts
@@ -11,16 +11,12 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
-  env: {
-    GEMINI_API_KEY: "PLACEHOLDER_API_KEY",
-    MONGO_URI: "mongodb+srv://zubairanwar245_db_user:dKB52OQEFvyQvEK5@cluster0.h7jabyi.mongodb.net/samfabrics",
-
-    IMAGEKIT_PUBLIC_KEY: "public_C+eb90mUDk9SaKV/4otCfze+N+g=",
-    IMAGEKIT_PRIVATE_KEY: "private_vXrOlOMEjxCuucJvyTPoidmZZMw=",
-    IMAGEKIT_URL_ENDPOINT: "https://ik.imagekit.io/samfabrics"
-  }
 };
 
 export default nextConfig;
